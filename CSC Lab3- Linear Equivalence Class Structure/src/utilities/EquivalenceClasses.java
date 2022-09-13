cc176bab786aefefa717e005639a34dd7cd11046
SpyChicken00 <94644105+SpyChicken00@users.noreply.github.com>
2022-09-12 20:27:16 -0400

    Formatting
    Formatted Equivalence Classes Skeleton Code, added TODO checks for required methods. and added return stubs

--- CSC Lab3- Linear Equivalence Class Structure/src/utilities/EquivalenceClasses.java
+++ CSC Lab3- Linear Equivalence Class Structure/src/utilities/EquivalenceClasses.java
@@ -1,40 +1,52 @@
 package utilities;
-
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-
+/**
+ * A collection of Equivalence Classes stored in an arrayList
+ * 
+ * <p>Bugs: ???
+* 
+* @author Jace Rettig and Sally Stahl
+* @Date 9-12-22
+* @param <T>
+*/
 public class EquivalenceClasses<T> {
 	protected Comparator<T> _comparator;
-
 	protected List<LinkedEquivalenceClass<T>> _rest;
 
 	public EquivalenceClasses(Comparator<T> comparator) {
-
+		_comparator = comparator;
+		_rest = new ArrayList<LinkedEquivalenceClass<T>>();
 	}
 
 	public boolean add(T element) {
-
+		//TODO
+		return false;
 	}
 
 	public boolean contains(T target) {
-
+		//TODO
+		return false;
 	}
 
-
 	public int size() {
-
+		//TODO
+		return -1;
 	}
 
 	public int numClasses() {
-
+		//TODO
+		return -1;
 	}
 
-
 	protected int indexOfClass(T element) {
-
+		//TODO
+		return -1;
 	}
 
 	public String toString() {
-
+		//TODO
+		return "ERROR";
 	}
 }
--- CSC Lab3- Linear Equivalence Class Structure/src/utilities/LinkedEquivalenceClass.java
+++ CSC Lab3- Linear Equivalence Class Structure/src/utilities/LinkedEquivalenceClass.java
@@ -1,74 +1,79 @@
 package utilities;
 import java.util.Comparator;
+/**
+ * A LinkedEquivalence Class, determines if two objects
+ * can be compared based on a specified criteria or something 
+ * idk fix this later
+ * 
+ * <p>Bugs: ???
+* 
+* @author Jace Rettig and Sally Stahl
+* @Date 9-12-22
+* @param <T>
+*/
 public class LinkedEquivalenceClass<T> {
 	
 	protected T _canoncial;
-	
-	
 	protected Comparator<T> _comparator;
-	
-	
-	
 	protected LinkedList<T> _rest;
 	
-	
-	
-	
 	public LinkedEquivalenceClass(Comparator<T> comparator) {
-		
+		//TODO how to initialize generic value if not given in constructor
+		//_canoncial = 
+		_comparator = comparator;
+		_rest = new LinkedList<T>();
 	}
 	
 	public T canonical() {
+		//TODO
 		return null;
-		
 	}
 	
 	public void isEmpty() {
-		
+		//TODO
 	}
 	
 	public void clear() {
-		
+		//TODO
 	}
 	
 	public void clearNonCanonical() {
-		
+		//TODO
 	}
 	
 	public int size() {
-		
+		//TODO
+		return -1;
 	}
-	
-	
+
 	public boolean add(T element) {
-		
+		//TODO
+		return false;
 	}
-	
 	
 	public boolean contains(T target) {
-		
+		//TODO
+		return false;
 	}
 	
 	public boolean belongs(T target) {
-		
+		//TODO
+		return false;
 	}
-	
-	
 	
 	public boolean remove(T target) {
-		
+		//TODO
+		return false;
 	}
-	
 	
 	public boolean removeCanonical() {
-		
+		//TODO
+		return false;
 	}
 	
-	
 	public boolean demoteAndSetCanonical(T element) {
-		//test 1 
-		
-		
+		//TODO
+		return false;
 	}
 	
 	

328b953ffcaf911f0a91bc60946f40dfab8b492b
sastahl28 <112001400+sastahl28@users.noreply.github.com>
2022-09-16 14:11:50 -0400

    Test add almost implemented

--- CSC Lab3- Linear Equivalence Class Structure/src/utilities/EquivalenceClasses.java
+++ CSC Lab3- Linear Equivalence Class Structure/src/utilities/EquivalenceClasses.java
@@ -2,9 +2,7 @@ package utilities;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * A collection of Equivalence Classes stored in an arrayList
--- CSC Lab3- Linear Equivalence Class Structure/test/utilities/EquivalenceClassesTest.java
+++ CSC Lab3- Linear Equivalence Class Structure/test/utilities/EquivalenceClassesTest.java
@@ -2,41 +2,114 @@ package utilities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Comparator;
+
 import org.junit.jupiter.api.Test;
 
 class EquivalenceClassesTest {
+	
+	EquivalenceClasses<LinkedEquivalenceClass<Integer>> _ec;
+	
+	
+	public EquivalenceClassesTest(){
 
+	Comparator<LinkedEquivalenceClass<Integer>> c = new Comparator<LinkedEquivalenceClass<Integer>>() {
+		// All even integers are 'equivalent' // All odd integers are 'equivalent'
+		public int compare(LinkedEquivalenceClass<Integer> x, LinkedEquivalenceClass<Integer> y) {
+			return x.equals(y) ? 0 : 1; }
+		};
+		
+		_ec = new EquivalenceClasses<LinkedEquivalenceClass<Integer>>(c);
+		
+		
+		
+	}
+		
+	
+	
 	@Test
 	void testAdd() {
-		fail("Not yet implemented");
+		//create the comparator for the linked equivalence classes
+		Comparator<Integer> c1 = new Comparator<Integer>() {
+			// All even integers are 'equivalent' // All odd integers are 'equivalent'
+			public int compare(Integer x, Integer y) {
+				return x % 2 == y % 2 ? 0 : 1; }
+			};
+		
+		//create a test linker equivalence class to use for testing purposes
+		LinkedEquivalenceClass<Integer>test1 = new LinkedEquivalenceClass<Integer>(c1);
+		LinkedEquivalenceClass<Integer>test2 = new LinkedEquivalenceClass<Integer>(c1);
+		
+		
+		//test with a null element
+		assertFalse(_ec.add(null));
+		
+		//add an element
+		assertTrue(_ec.add(test1));
+		
+		//check that it is contains within that
+		//assertTrue(_ec.contains(test1));
+		
+		//add another element
+		assertTrue(_ec.add(test2));
+		
+		//check that new element is contains
+		//assertTrue(_ec.contains(test2));
+	
+		
+		
+		
 	}
 	
 	
 	@Test
 	void testContains() {
-		fail("Not yet implemented");
+		
+		//test with a null element
+		
+		//add elements to the list
+		
+		//test contains with an element in the lsit
+		
+		//test with an element that is not in the list
+		
+		//test with a null element again
+		
+		
 	}
 	
 	
 	@Test
 	void testSize() {
-		fail("Not yet implemented");
+	
+		//check on an array with no elements
+		
+		//add elements
+		
+		//check the size again
+		
+		//add more elements
+		
+		//check the size again
+		
 	}
+	
+	
 	
 	@Test
 	void testNumClasses() {
-		fail("Not yet implemented");
+		
 	}
 	
 	
 	@Test
 	void testIndexOfClasses() {
-		fail("Not yet implemented");
+		
 	}
 	
 	@Test
 	void testToString() {
-		fail("Not yet implemented");
+		
 	}
 	
 }

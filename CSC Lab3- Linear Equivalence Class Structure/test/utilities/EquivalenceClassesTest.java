8120492a5cb6c71174f46cb504e1cbbc06fc9558
sastahl28 <112001400+sastahl28@users.noreply.github.com>
2022-09-16 14:35:05 -0400

    Changes test variables

--- CSC Lab3- Linear Equivalence Class Structure/test/utilities/EquivalenceClassesTest.java
+++ CSC Lab3- Linear Equivalence Class Structure/test/utilities/EquivalenceClassesTest.java
@@ -11,6 +11,11 @@ class EquivalenceClassesTest {
 	EquivalenceClasses<LinkedEquivalenceClass<Integer>> _ec;
 
 
+	LinkedEquivalenceClass<Integer> _test1;
+	LinkedEquivalenceClass<Integer> _test2;
+	LinkedEquivalenceClass<Integer> _test3;
+
+
 	public EquivalenceClassesTest(){
 
 		Comparator<LinkedEquivalenceClass<Integer>> c = new Comparator<LinkedEquivalenceClass<Integer>>() {
@@ -22,6 +27,18 @@ class EquivalenceClassesTest {
 		_ec = new EquivalenceClasses<LinkedEquivalenceClass<Integer>>(c);
 
 
+		//create the test equivalence classes
+		Comparator<Integer> c1 = new Comparator<Integer>() {
+			// All even integers are 'equivalent' // All odd integers are 'equivalent'
+			public int compare(Integer x, Integer y) {
+				return x % 2 == y % 2 ? 0 : 1; }
+		};
+
+
+
+		_test1 = new LinkedEquivalenceClass<Integer>(c1);
+		_test2 = new LinkedEquivalenceClass<Integer>(c1);
+		_test3 = new LinkedEquivalenceClass<Integer>(c1);
 	}
 
 
@@ -32,29 +49,17 @@ class EquivalenceClassesTest {
 		t.demoteAndSetCanonical(x);
 		t.add(y);
 		t.add(z);
-
 	}
 
 
 
 	@Test
 	void testAdd() {
-		//create the test equivalence classes
-		Comparator<Integer> c1 = new Comparator<Integer>() {
-			// All even integers are 'equivalent' // All odd integers are 'equivalent'
-			public int compare(Integer x, Integer y) {
-				return x % 2 == y % 2 ? 0 : 1; }
-		};
-
-		//create test equivalence classes
-		LinkedEquivalenceClass<Integer>test1 = new LinkedEquivalenceClass<Integer>(c1);
-		LinkedEquivalenceClass<Integer>test2 = new LinkedEquivalenceClass<Integer>(c1);
-		LinkedEquivalenceClass<Integer>test3 = new LinkedEquivalenceClass<Integer>(c1);
 
 		//add elements to the linker equivalence classes
-		addElements(test1 , 2, 4, 6);
-		addElements(test2 , 4, 8, 12);
-		addElements(test3 , 6, 12 , 18);
+		addElements(_test1 , 2, 4, 6);
+		addElements(_test2 , 4, 8, 12);
+		addElements(_test3 , 6, 12 , 18);
 
 
 
@@ -62,16 +67,16 @@ class EquivalenceClassesTest {
 		assertFalse(_ec.add(null));
 
 		//add an element
-		assertTrue(_ec.add(test1));
+		assertTrue(_ec.add(_test1));
 
 		//check that it is contains within that
-		assertTrue(_ec.contains(test1));
+		assertTrue(_ec.contains(_test1));
 
 		//add another element
-		assertTrue(_ec.add(test2));
+		assertTrue(_ec.add(_test2));
 
 		//check that new element is contains
-		assertTrue(_ec.contains(test2));
+		assertTrue(_ec.contains(_test2));
 
 
 
@@ -81,40 +86,29 @@ class EquivalenceClassesTest {
 
 	@Test
 	void testContains() {
-		//create the test equivalence classes
-		Comparator<Integer> c1 = new Comparator<Integer>() {
-			// All even integers are 'equivalent' // All odd integers are 'equivalent'
-			public int compare(Integer x, Integer y) {
-				return x % 2 == y % 2 ? 0 : 1; }
-		};
-
-		//create test equivalence classes
-		LinkedEquivalenceClass<Integer>test1 = new LinkedEquivalenceClass<Integer>(c1);
-		LinkedEquivalenceClass<Integer>test2 = new LinkedEquivalenceClass<Integer>(c1);
-		LinkedEquivalenceClass<Integer>test3 = new LinkedEquivalenceClass<Integer>(c1);
 
 		//add elements to the linker equivalence classes
-		addElements(test1 , 2, 4, 6);
-		addElements(test2 , 4, 8, 12);
-		addElements(test3 , 6, 12 , 18);
+		addElements(_test1 , 2, 4, 6);
+		addElements(_test2 , 4, 8, 12);
+		addElements(_test3 , 6, 12 , 18);
 
 		//test with a null element
 		assertFalse(_ec.contains(null));
 
 		//add an element
-		assertTrue(_ec.add(test1));
+		assertTrue(_ec.add(_test1));
 
 		//check that it is contains within that
-		assertTrue(_ec.contains(test1));
+		assertTrue(_ec.contains(_test1));
 
 		//add another element
-		assertTrue(_ec.add(test2));
+		assertTrue(_ec.add(_test2));
 
 		//check that new element is contains
-		assertTrue(_ec.contains(test2));
+		assertTrue(_ec.contains(_test2));
 
 		//test with an element that is not in the list
-		assertFalse(_ec.contains(test3));
+		assertFalse(_ec.contains(_test3));
 
 
 
@@ -123,41 +117,30 @@ class EquivalenceClassesTest {
 
 	@Test
 	void testSize() {
-		//create the test equivalence classes
-		Comparator<Integer> c1 = new Comparator<Integer>() {
-			// All even integers are 'equivalent' // All odd integers are 'equivalent'
-			public int compare(Integer x, Integer y) {
-				return x % 2 == y % 2 ? 0 : 1; }
-		};
-
-		//create test equivalence classes
-		LinkedEquivalenceClass<Integer>test1 = new LinkedEquivalenceClass<Integer>(c1);
-		LinkedEquivalenceClass<Integer>test2 = new LinkedEquivalenceClass<Integer>(c1);
-		LinkedEquivalenceClass<Integer>test3 = new LinkedEquivalenceClass<Integer>(c1);
 
 		//add elements to the linker equivalence classes
-		addElements(test1 , 2, 4, 6);
-		addElements(test2 , 4, 8, 12);
-		addElements(test3 , 6, 12 , 18);
+		addElements(_test1 , 2, 4, 6);
+		addElements(_test2 , 4, 8, 12);
+		addElements(_test3 , 6, 12 , 18);
 
 		//test with no elements in the set
 		assertEquals(0 , _ec.size());
 
 		//add an element
-		assertTrue(_ec.add(test1));
+		assertTrue(_ec.add(_test1));
 
 		//check the size of the class
 		assertEquals(1 , _ec.size());
 
 		//add another element
-		assertTrue(_ec.add(test2));
+		assertTrue(_ec.add(_test2));
 
 		//check the size 
 		assertEquals( 2 , _ec.size());
 
 		//add an element that already exists 
-		assertFalse(_ec.add(test2));
-		
+		assertFalse(_ec.add(_test2));
+
 		//check that the size is still 2
 		assertEquals( 2 , _ec.size());
 
